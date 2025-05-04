e83a791b11da3b0da7d5aebec2647a83932fae63
cspadua <carlpadua1217@gmail.com>
2025-05-04 14:03:13 +0800

    added A04

---
+++ ADET/A04/script.js
@@ -0,0 +1,428 @@
+
+var products = [
+  {
+    "category": "Espresso",
+    "categoryImage": "img/espresso.svg",
+    "contents": [
+      {
+        "isAvailable": true,
+        "name": "Caffe Misto",
+        "code": "CFM",
+        "contentImage": "img/espressoImg/1.png",
+        "price": 120
+      },
+      {
+        "isAvailable": true,
+        "name": "Flat White",
+        "code": "FTW",
+        "contentImage": "img/espressoImg/2.png",
+        "price": 110
+      },
+      {
+        "isAvailable": true,
+        "name": "Espresso",
+        "code": "ESP",
+        "contentImage": "img/espressoImg/3.png",
+        "price": 100
+      },
+      {
+        "isAvailable": true,
+        "name": "Con Panna",
+        "code": "CPN",
+        "contentImage": "img/espressoImg/4.png",
+        "price": 130
+      }
+    ]
+  },
+  {
+    "category": "Refreshers",
+    "categoryImage": "img/refreshers.svg",
+    "contents": [
+      {
+        "isAvailable": true,
+        "name": "Strawberry Açaí",
+        "code": "STA",
+        "contentImage": "img/refreshersImg/1.png",
+        "price": 125
+      },
+      {
+        "isAvailable": true,
+        "name": "Strawberry Lemonade​",
+        "code": "STL",
+        "contentImage": "img/refreshersImg/2.png",
+        "price": 140
+      },
+      {
+        "isAvailable": true,
+        "name": "Mango Dragonfruit",
+        "code": "MGD",
+        "contentImage": "img/refreshersImg/3.png",
+        "price": 115
+      },
+      {
+        "isAvailable": true,
+        "name": "Dragon Drink",
+        "code": "DRD",
+        "contentImage": "img/refreshersImg/4.png",
+        "price": 130
+      }
+    ]
+  },
+  {
+    "category": "Cold Brew",
+    "categoryImage": "img/coldBrew.svg",
+    "contents": [
+      {
+        "isAvailable": true,
+        "name": "Salted Caramel",
+        "code": "STC",
+        "contentImage": "img/coldBrewImg/1.png",
+        "price": 140
+      },
+      {
+        "isAvailable": true,
+        "name": "Vanilla Sweet Cream​",
+        "code": "VSC",
+        "contentImage": "img/coldBrewImg/2.png",
+        "price": 135
+      },
+      {
+        "isAvailable": true,
+        "name": "Nitro Cold Brew",
+        "code": "NCW",
+        "contentImage": "img/coldBrewImg/3.png",
+        "price": 125
+      },
+      {
+        "isAvailable": true,
+        "name": "Cold Brew",
+        "code": "CB",
+        "contentImage": "img/coldBrewImg/4.png",
+        "price": 100
+      },
+      {
+        "isAvailable": true,
+        "name": "Nitro Sweet Cream",
+        "code": "NTW",
+        "contentImage": "img/coldBrewImg/5.png",
+        "price": 150
+      },
+      {
+        "isAvailable": true,
+        "name": "Dark Caramel Nitro",
+        "code": "DCN",
+        "contentImage": "img/coldBrewImg/6.png",
+        "price": 130
+      }
+    ]
+  },
+  {
+    "category": "Desserts",
+    "categoryImage": "img/dessert.svg",
+    "contents": [
+      {
+        "isAvailable": true,
+        "name": "Chocolate Mousse",
+        "code": "CTM",
+        "contentImage": "img/dessertsImg/1.png",
+        "price": 850
+      },
+      {
+        "isAvailable": true,
+        "name": "New York Cheesecake​",
+        "code": "NYC",
+        "contentImage": "img/dessertsImg/2.png",
+        "price": 160
+      },
+      {
+        "isAvailable": true,
+        "name": "Blueberry Licious",
+        "code": "BBL",
+        "contentImage": "img/dessertsImg/3.png",
+        "price": 170
+      },
+      {
+        "isAvailable": true,
+        "name": "Classic Chocolate Cake",
+        "code": "CCC",
+        "contentImage": "img/dessertsImg/4.png",
+        "price": 190
+      },
+      {
+        "isAvailable": true,
+        "name": "Banoffee Pie",
+        "code": "BFP",
+        "contentImage": "img/dessertsImg/5.png",
+        "price": 145
+      }
+    ]
+  },
+  {
+    "category": "Bakery",
+    "categoryImage": "img/bakery.svg",
+    "contents": [
+      {
+        "isAvailable": true,
+        "name": "Belgian Waffle",
+        "code": "BGW",
+        "contentImage": "img/bakeryImg/1.png",
+        "price": 95
+      },
+      {
+        "isAvailable": true,
+        "name": "Banana Bread",
+        "code": "BNB",
+        "contentImage": "img/bakeryImg/2.png",
+        "price": 110
+      },
+      {
+        "isAvailable": true,
+        "name": "Chocolate Doughnut",
+        "code": "CCD",
+        "contentImage": "img/bakeryImg/3.png",
+        "price": 125
+      },
+      {
+        "isAvailable": true,
+        "name": "Cinnamon Danish",
+        "code": "CND",
+        "contentImage": "img/bakeryImg/4.png",
+        "price": 140
+      },
+      {
+        "isAvailable": true,
+        "name": "Macadamia Cookie",
+        "code": "MDC",
+        "contentImage": "img/bakeryImg/5.png",
+        "price": 135
+      },
+      {
+        "isAvailable": true,
+        "name": "Pistachio Dream Bar",
+        "code": "PDB",
+        "contentImage": "img/bakeryImg/6.png",
+        "price": 205
+      },
+      {
+        "isAvailable": true,
+        "name": "Sausage Roll",
+        "code": "SGR",
+        "contentImage": "img/bakeryImg/7.png",
+        "price": 155
+      }
+    ]
+  },
+  {
+    "category": "Pasta",
+    "categoryImage": "img/pasta.svg",
+    "contents": [
+      {
+        "isAvailable": true,
+        "name": "Pasta Napolitana",
+        "code": "PSN",
+        "contentImage": "img/pastaImg/1.png",
+        "price": 132
+      }
+    ]
+  },
+  {
+    "category": "Sandwiches",
+    "categoryImage": "img/sandwich.svg",
+    "contents": [
+      {
+        "isAvailable": true,
+        "name": "Classic, Egg & Cheese",
+        "code": "CEC",
+        "contentImage": "img/sandwichImg/1.png",
+        "price": 70
+      },
+      {
+        "isAvailable": true,
+        "name": "Ham, Egg, & Cheese",
+        "code": "HEC",
+        "contentImage": "img/sandwichImg/2.png",
+        "price": 80
+      },
+      {
+        "isAvailable": true,
+        "name": "Spanish Chorizo",
+        "code": "SNC",
+        "contentImage": "img/sandwichImg/3.png",
+        "price": 75
+      },
+      {
+        "isAvailable": true,
+        "name": "Fontina Cheese",
+        "code": "FTC",
+        "contentImage": "img/sandwichImg/4.png",
+        "price": 90
+      }
+    ]
+  },
+  {
+    "category": "Cake",
+    "categoryImage": "img/cake.svg",
+    "contents": [
+      {
+        "isAvailable": true,
+        "name": "Blackout Cake",
+        "code": "BOC",
+        "contentImage": "img/cakeImg/1.png",
+        "price": 900
+      },
+      {
+        "isAvailable": true,
+        "name": "Peanut Butter Cheesecake",
+        "code": "PBC",
+        "contentImage": "img/cakeImg/2.png",
+        "price": 1100
+      },
+    ]
+  },
+  {
+    "category": "Tart",
+    "categoryImage": "img/tart.svg",
+    "contents": [
+      {
+        "isAvailable": true,
+        "name": "Strawberry Tart",
+        "code": "STT",
+        "contentImage": "img/tartImg/1.png",
+        "price": 400
+      },
+      {
+        "isAvailable": true,
+        "name": "Tarte Au Chocolate",
+        "code": "PBC",
+        "contentImage": "img/tartImg/2.png",
+        "price": 470
+      },
+      {
+        "isAvailable": true,
+        "name": "Meringue Tartlet",
+        "code": "MRT",
+        "contentImage": "img/tartImg/3.png",
+        "price": 520
+      },
+    ]
+  },
+  {
+    "category": "Tea",
+    "categoryImage": "img/tea.svg",
+    "contents": [
+      {
+        "isAvailable": true,
+        "name": "Full Brewed Tea",
+        "code": "FBT",
+        "contentImage": "img/teaImg/1.png",
+        "price": 79
+      }
+    ]
+  }
+]
+
+var total = 0;
+
+function loadCategories() {
+  var categoriesContainer = document.getElementById("categories");
+
+  products.forEach((product, index) => {
+  categoriesContainer.innerHTML += `
+      <div onclick="loadProducts(` + index + `)" class="shadow-sm rounded-4 mx-1 custom-button p-3 text-center d-flex flex-column align-items-center justify-content-center">
+        <img src="` + product.categoryImage + `" class="img-fluid mb-2" style="height: 25px;">
+        <small>` + product.category + `</small>
+      </div>
+    `;
+  });
+}
+
+function loadProducts(categoryIndex) {
+  var maincontainer = document.getElementById("maincontainer");
+  maincontainer.innerHTML = "";
+
+  var categoryName = products[categoryIndex].category;
+  maincontainer.innerHTML += `<h4 class="my-3 w-100 text-start mb-3" style="font-family: 'Alexandria', sans-serif; color: #1E3930;">` + categoryName + ` Menu</h4>`;
+  {
+    products[categoryIndex].contents.forEach(content => {
+      maincontainer.innerHTML += `
+      <div class="col-12 col-md-6 mb-4">
+          <div class="card">
+              <img src="` + content.contentImage + `" class="card-img-top">
+              <div class="card-body">
+                  <div class="d-flex justify-content-between align-items-start mb-1">
+                      <div>
+                          <h5 class="card-title mb-1">` + content.name + `</h5>
+                      </div>
+                      <h5 class="card-text mb-0">₱` + content.price + `</h5>
+                  </div>
+                  <div class="text-center mt-4">
+                      <button id="contentBtn" onclick="addToReceipt('` + content.price + `','` + content.code + `')" class="btn btn-dark">Add to Billing</button>
+                  </div>
+              </div>
+          </div>
+      </div>
+      `;
+    
+    });
+  }
+
+}
+
+window.onload = function() {
+  loadProducts(0);
+};
+
+function addToReceipt(price, code) {
+  var receiptContainer = document.getElementById("receipt");
+  total = parseFloat(total) + parseFloat(price);
+
+  totalValueElement = document.getElementById("totalValue");
+  totalValueElement.innerHTML = "₱" + total;
+
+  receiptContainer.innerHTML += `
+    <div class="m-0 d-flex flex-row justify-content-between">
+      <div class="mt-2 peso">`+ code + `</div>
+      <div class="mt-2 peso">₱`+ price + `</div>
+    </div>
+  `;
+}
+
+loadCategories();
+
+
+function updateTaxAndFinalPrice() {
+  const taxRate = 0.01;
+  const taxAmount = total * taxRate;
+  const finalPrice = total + taxAmount;
+
+  document.getElementById("taxValue").innerText = "₱" + taxAmount.toFixed(2);
+  document.getElementById("finalPrice").innerText = "₱" + finalPrice.toFixed(2);
+}
+
+function addToReceipt(price, code) {
+  var receiptContainer = document.getElementById("receipt");
+  total = parseFloat(total) + parseFloat(price);
+
+  var totalValueElement = document.getElementById("totalValue");
+  totalValueElement.innerHTML = "₱" + total.toFixed(2);
+
+  receiptContainer.innerHTML += `
+    <div class="m-0 d-flex flex-row justify-content-between">
+      <div class="mt-2 peso">` + code + `</div>
+      <div class="mt-2 peso">₱` + parseFloat(price).toFixed(2) + `</div>
+    </div>
+  `;
+
+  updateTaxAndFinalPrice();
+}
+
+document.getElementById('resetBtn').addEventListener('click', function () {
+  // Clear receipt items
+  document.getElementById('receipt').innerHTML = '';
+
+  // Reset total values
+  document.getElementById('totalValue').textContent = '₱0';
+  document.getElementById('taxValue').textContent = '₱0';
+  document.getElementById('finalPrice').textContent = '₱0.00';
+
+});
